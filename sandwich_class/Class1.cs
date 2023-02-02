c430a34e19e0e18718985f6b22657addafbb7888
AlexanderAcc <124236169+AlexanderAcc@users.noreply.github.com>
2023-02-02 20:29:35 +0700

    first commit

---
+++ sandwich_class/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sandwich_class
+{
+    class Sandwich // бутерброд
+    {
+        public int Weight { get; set; } // масса
+
+    }
+    class Bread // хлеб
+    {
+
+        public int Weight { get; set; } // масса
+
+        public static Sandwich operator +(Butter butter, Bread bread) // суммирование массы хлеба и масла
+        {
+            return new Sandwich { Weight = butter.Weight + bread.Weight }; // возвращение значения в форму
+        }
+    }
+    class Butter // масло
+    {
+
+        public int Weight { get; set; } // масса
+
+        public static Sandwich operator +(Bread bread, Butter butter) // суммирование массы хлеба и масла
+        {
+            return new Sandwich { Weight = butter.Weight + bread.Weight }; // возвращение значения в форму
+        }
+    }
+
+}
---
+++ sandwich_class/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace sandwich_class
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Bread bread = new Bread { Weight = Convert.ToInt32(text1.Text) }; // объект класса хлеб с текстом из textbox
+
+            Butter butter = new Butter { Weight = Convert.ToInt32(text2.Text) }; // объект класса хлеб с текстом из textbox
+
+            Sandwich sandwich = bread + butter; // создание объекта класса бутерброд складывая массу хлеба и масла
+
+            lb.Content = sandwich.Weight + "г"; // передача в label
+        }
+    }
+}
